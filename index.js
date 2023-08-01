fe807306ef77764ab626b6a8ecd2df3a3670965f
Ralf303 <nikitasuhanov34@gmail.com>
2023-08-01 22:54:16 +0300

    обновленный сервер

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ import cors from "cors";
 
 import userRouter from "./routers/userRouter.js";
 import logsRouter from "./routers/logsRouter.js";
-import bodyParser from "body-parser";
+
 
 const PORT = 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
 app.use("/users", userRouter);
 app.use("/logs", logsRouter);
 
