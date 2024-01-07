927507a8f4bebb2dd115bded3d3cdb9e47f5ab73
ralf303 <nikitasuhanov34@gmail.com>
2024-01-07 10:59:13 +0300

    https fix

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
+import https from "https";
+import fs from "fs";
 
 import userRouter from "./routers/userRouter.js";
 import logsRouter from "./routers/logsRouter.js";
-
 
 const PORT = 5000;
 const app = express();
@@ -14,6 +15,10 @@ app.use("/users", userRouter);
 app.use("/logs", logsRouter);
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const options = {
+  key: fs.readFileSync(process.env.SECRET_KEY),
+  cert: fs.readFileSync(process.env.SERTIFICATE),
+};
+https.createServer(options, app).listen(PORT, () => {
+  console.log("сервер работает на порту", PORT);
 });
